8a584e9f93a0870f37e48ddfc7970c33cf9d62b3
Ian <iandcampbell94@gmail.com>
2017-10-10 23:19:16 -0400

    nothing

--- Labs/Song/DEMO_MyCircularLinkedList.java
+++ Labs/Song/DEMO_MyCircularLinkedList.java
@@ -5,15 +5,15 @@ public class DEMO_MyCircularLinkedList {
       
       MyCircularLinkedList<Song> list = new MyCircularLinkedList<Song>();
             
-      list.add(new Song("Queen", "Killer Queen", 1));
+      //list.add(new Song("Queen", "Killer Queen", 1));
       
       list.add(0, new Song("TMBG", "Older", 3));
-      list.add(1, new Song("1TMBG", "Older", 3));
-      list.add(2, new Song("2TMBG", "Older", 3));
-      list.add(3, new Song("3TMBG", "Older", 3));
-      
-      list.remove(new Song("TMBG", "Older", 3));
-      
+      list.add(1, new Song("1TMBG", "COlder", 3));
+      list.add(2, new Song("2TMBG", "BOlder", 3));
+      list.add(3, new Song("3TMBG", "FOlder", 3));
+      System.out.println(list.remove(0));
+      //System.out.println(list.remove(new Song("TMBG", "Older", 3)));
+      /*
       list.set(0, new Song("Smash Mouth", "All Stars", 2));
       
       System.out.println(list.subList(0, 2).get(0).toString());
@@ -21,7 +21,7 @@ public class DEMO_MyCircularLinkedList {
       list.shift(2);
       
       System.out.println();
-      
+      */
       for (int i = 0; i < list.size; i++) {
          System.out.println(list.get(i).getArtist() + ", " +
                             list.get(i).getSongName() + ", " +
